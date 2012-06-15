650629d2b7ae891ecf2600f5c34482eb385fd51e
Scott Duncombe <srduncombe@gmail.com>
2012-06-15 16:52:53 -0700

    Templates can now bind to model function __addTemplates and are executed by bind()

--- table.js
+++ table.js
@@ -1,5 +1,5 @@
 tableModel = function(rows, fields) {
-	var tableOptions = {
+	this.__options = {
 		field: {
 			name: 'name',
 			data: 'name',
@@ -11,12 +11,12 @@ tableModel = function(rows, fields) {
 	
 	// Templates
 	this.__templates = ko.observable({
-		table: '<div class="header"><div class="inner" data-bind="style: { width: (fields.__width()+'+tableOptions.ender+')+\'px\' }, template: { name: \'header\', foreach: fields() }"><div class="ender" style="width: '+tableOptions.ender+'px"></div></div></div>'+
+		table: '<div class="header"><div class="inner" data-bind="style: { width: (fields.__width()+'+this.__options.ender+')+\'px\' }, template: { name: \'header\', foreach: fields() }"><div class="ender" style="width: '+this.__options.ender+'px"></div></div></div>'+
 			'<div class="body" data-bind="syncScroll: \'.header\'"><div class="inner" data-bind="template: {name: \'row\', foreach: rows.__trimmed() }, fillTable: true, style: { width: fields.__width()+\'px\' }" ></div></div>',
-		header: '<div class="title entry" data-bind="text: $data.'+tableOptions.field.name+', setTemplateClass: $data.'+tableOptions.field.type+'"></div>',
+		header: '<div class="title entry" data-bind="text: $data.'+this.__options.field.name+', setTemplateClass: $data.'+this.__options.field.type+'"></div>',
 		row: '<div class="row" data-bind="template: {name: \'entry\', foreach: $parent.fields() }"></div>',
-		entry: '<div class="entry" data-bind="entryTemplate: {field: $data, row: $parent }, setTemplateClass: $data.'+tableOptions.field.type+'"></div>',
-		text: '<textarea data-bind="value: $parent[$data.'+tableOptions.field.data+']"></textarea>'
+		entry: '<div class="entry" data-bind="entryTemplate: {field: $data, row: $parent }, setTemplateClass: $data.'+this.__options.field.type+'"></div>',
+		text: '<textarea data-bind="value: $parent[$data.'+this.__options.field.data+']"></textarea>'
 	})
 	this.__widths = ko.observable({})
 	
@@ -35,23 +35,60 @@ tableModel = function(rows, fields) {
 	this.fields.__width = ko.computed(function() { 
 		var width = 0, fields = this.fields(), widths = this.__widths()
 		for (var i=0; i < fields.length; i++) {
-			if( typeof widths[ fields[i][ tableOptions.field.type] ] == 'undefined' ) width += 100
-			else width += widths[ fields[i][ tableOptions.field.type] ]
+			if( typeof widths[ fields[i][ this.__options.field.type] ] == 'undefined' ) width += 100
+			else width += widths[ fields[i][ this.__options.field.type] ]
 			width += 4
 		};
 		return width
 	},this)
 
-	this.addTemplate = function(name, template, width) {
+	this.__addTemplate = function(obj) {
 		var templates = ko.toJS(this.__templates ), widths = ko.toJS(this.__widths )
-		if( typeof template != 'undefined' ) {
-			templates[name] = template.replace(/{data_field}/g,tableOptions.field.data)
+		if( typeof obj.template != 'undefined' ) {
+			templates[obj.name] = obj.template.replace(/{data_field}/g,this.__options.field.data).replace(/{option_field}/g,this.__options.field.options)
 			this.__templates(templates)
 		}
-		if( typeof width != 'undefined' ) {
-			widths[name] = width
-			this.__widths(widths)
+		if( typeof obj.width != 'undefined' ) {
+			widths[obj.name] = obj.width
+			this.__widths( widths)
 		}
+	}
+	this.bind = function(no_ko) {
+		
+		ko.templateSources.stringTemplate = function(template, templates) {
+			this.templateName = template;
+			this.templates = templates;
+		}
+
+		ko.utils.extend(ko.templateSources.stringTemplate.prototype, {
+			data: function(key, value) {
+				this.templates._data = this.templates._data || {};
+				this.templates._data[this.templateName] = this.templates._data[this.templateName] || {};
+				if (arguments.length === 1)  return this.templates._data[this.templateName][key]; 
+				this.templates._data[this.templateName][key] = value;
+			},
+			text: function(value) {
+				if (arguments.length === 0) return this.templates[this.templateName]; 
+				this.templates[this.templateName] = value;   
+			},
+			foreach: function(value) {
+				return '...'
+			}
+		});
+
+		//modify an existing templateEngine to work with string templates
+		function createStringTemplateEngine(templateEngine, templates) {
+			templateEngine.makeTemplateSource = function(template) { return new ko.templateSources.stringTemplate(template, templates); }   
+			return templateEngine;
+		}
+
+		if( typeof this.constructor.__addedTemplates != 'undefined' ) {
+			for (var i=0; i < this.constructor.__addedTemplates.length; i++) {
+				this.__addTemplate( this.constructor.__addedTemplates[i] )
+			};
+		}
+		ko.setTemplateEngine(createStringTemplateEngine(new ko.nativeTemplateEngine(), this.__templates()));
+		if( !no_ko ) ko.applyBindings( this );
 	}
 
 
@@ -75,37 +112,20 @@ tableModel = function(rows, fields) {
 				field = options.field
 				other = allBindingsAccessor(),
 				ctx = bindingContext,
-				template = typeof ctx.$root.__templates()[ field[tableOptions.field.type] ] != 'undefined' ? field[tableOptions.field.type] : 'text'
+				template = typeof ctx.$root.__templates()[ field[bindingContext.$root.__options.field.type] ] != 'undefined' ? field[ bindingContext.$root.__options.field.type] : 'text'
 			other.template = {name: template }
 		}
 	}
 
 }
 
-
-ko.templateSources.stringTemplate = function(template, templates) {
-	this.templateName = template;
-	this.templates = templates;
-}
-
-ko.utils.extend(ko.templateSources.stringTemplate.prototype, {
-	data: function(key, value) {
-		this.templates._data = this.templates._data || {};
-		this.templates._data[this.templateName] = this.templates._data[this.templateName] || {};
-		if (arguments.length === 1)  return this.templates._data[this.templateName][key]; 
-		this.templates._data[this.templateName][key] = value;
-	},
-	text: function(value) {
-		if (arguments.length === 0) return this.templates[this.templateName]; 
-		this.templates[this.templateName] = value;   
-	},
-	foreach: function(value) {
-		return '...'
+tableModel.__addTemplate = function(name,template,width) {
+	templateObject = {name: name}
+	if( template != undefined ) templateObject.template = template
+	if( template != undefined ) templateObject.width = width
+	if( typeof this.__addedTemplates == 'undefined' ) {
+		this.__addedTemplates = [ templateObject ]
+	} else {
+		this.__addedTemplates.push( templateObject )
 	}
-});
-
-//modify an existing templateEngine to work with string templates
-function createStringTemplateEngine(templateEngine, templates) {
-	templateEngine.makeTemplateSource = function(template) { return new ko.templateSources.stringTemplate(template, templates); }   
-	return templateEngine;
 }
