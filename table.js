9465bb2777155f5d87a7261ad8faa52fffd4a8ce
Scott Duncombe <srduncombe@gmail.com>
2012-06-15 15:43:00 -0700

    Moving string tempting system into table.js

--- table.js
+++ table.js
@@ -169,4 +169,30 @@ tableModel = function(rows, fields) {
 		return width
 	},this)
 
+
+ko.templateSources.stringTemplate = function(template, templates) {
+	this.templateName = template;
+	this.templates = templates;
+}
+
+ko.utils.extend(ko.templateSources.stringTemplate.prototype, {
+	data: function(key, value) {
+		this.templates._data = this.templates._data || {};
+		this.templates._data[this.templateName] = this.templates._data[this.templateName] || {};
+		if (arguments.length === 1)  return this.templates._data[this.templateName][key]; 
+		this.templates._data[this.templateName][key] = value;
+	},
+	text: function(value) {
+		if (arguments.length === 0) return this.templates[this.templateName]; 
+		this.templates[this.templateName] = value;   
+	},
+	foreach: function(value) {
+		return '...'
+	}
+});
+
+}//modify an existing templateEngine to work with string templates
+function createStringTemplateEngine(templateEngine, templates) {
+	templateEngine.makeTemplateSource = function(template) { return new ko.templateSources.stringTemplate(template, templates); }   
+	return templateEngine;
 }
